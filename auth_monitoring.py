43790d030c209bf353ed55208d3b2ef7d9cedc8a
Wojtek <wzygadlo@users.noreply.github.com>
2017-03-14 15:01:40 +0000

    cosmetic changes

--- auth_monitoring.py
+++ auth_monitoring.py
@@ -12,19 +12,14 @@ def find_ip_address():
     loglist = logs.splitlines()
     ip_addr = []
     ip = []
-    # chain = []
     for index, message in enumerate(loglist):
         if 'Failed password' in message:
             ip_addr.append(re.findall(r'\b[0-9]+(?:\.[0-9]+){3}\b', message))
-    # return (ip_addr)
     uniq_ip = collections.Counter((list(itertools.chain(*ip_addr))))
     for key, value in uniq_ip.items():
         if int(value) > 4:
             ip.append(key)
     return ip
-    # uniq_ip = collections.Counter(chain2)
-    # chain = (list(set(itertools.chain(*ip_addr))))
-    # return chain
 
 
 def add_iptable_rules():
@@ -33,4 +28,5 @@ def add_iptable_rules():
         if ip not in subprocess.getoutput('sudo iptables -L'):
             subprocess.getoutput('sudo iptables -I INPUT -s {} -j DROP'.format(ip))
 
+            
 add_iptable_rules()
