9b83caf78156e6a6f7acf16973bb07bcd5eeda70
Wojtek <wzygadlo@users.noreply.github.com>
2017-06-14 22:20:49 +0100

    Create kde_blur.py

--- kde_blur.py
+++ kde_blur.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import subprocess
 
-blur = 'xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id '
 konsole_id = subprocess.getoutput('xdotool getactivewindow')
+blur = 'xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id '
 
 if konsole_id is not None:
     subprocess.getoutput(blur + konsole_id)
